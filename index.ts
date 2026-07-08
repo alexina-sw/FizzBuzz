c9126b1060e719c7b754d1265bb0f65f0067764c
alexina-sw <Alexia.Mocanu@softwire.com>
2026-07-08 13:51:17 +0300

    implement extended fizzbuzz

--- index.ts
+++ index.ts
@@ -1,15 +1,44 @@
 // This is our main function
 function fizzbuzz(): void {
+    const bang: string = "Bang";
+    const bong: string = "Bong";
+    const buzz: string = "Buzz";
+    const fezz: string = "Fezz";
+    const fizz: string = "Fizz";
+
+    const output: string[] = [];
+
     for (let i: number = 1; i <= 100; i ++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
+        if (i % 3 === 0) {
+            output.push(fizz);
         }
+        if (i % 5 === 0) {
+            output.push(buzz);
+        }
+        if (i % 7 === 0) {
+            output.push(bang);
+        }
+        if (i % 11 === 0) {
+            output.length = 0;
+            if (i % 13 === 0) {
+                output.push(fezz);
+            }
+            output.push(bong);
+        }
+        if (i % 13 === 0) {
+            const index = output.findIndex(word => word.startsWith("B"));
+            if (index === -1) {
+                output.push(fezz);
+            } else {
+                output.splice(index, 0, fezz);
+            }
+        }
+        if (i % 17 === 0) {
+            output.reverse();
+        }
+        const result: string = output.length === 0 ? i.toString() : output.join("");
+        console.log(result)
+        output.length = 0;
     }
 }
 

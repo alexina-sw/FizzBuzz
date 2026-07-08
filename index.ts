4b4fa37d2fcacda7ac22d4f02268c0b0125f3167
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-08 16:01:09 +0300

    added user specified command-line options

--- index.ts
+++ index.ts
@@ -15,7 +15,8 @@ function readInput(question: string): Promise<string> {
 }
 
 // This is our main function
-function fizzbuzz(n: number): void {
+function fizzbuzz(n: number, useFizz?: boolean, useBuzz?: boolean, useBang?: boolean, useBong?: boolean,
+    useFezz?: boolean, useReverse?: boolean): void {
     const bang: string = "Bang";
     const bong: string = "Bong";
     const buzz: string = "Buzz";
@@ -25,23 +26,23 @@ function fizzbuzz(n: number): void {
     const output: string[] = [];
 
     for (let i: number = 1; i <= n; i ++) {
-        if (i % 3 === 0) {
+        if (i % 3 === 0 && useFizz) {
             output.push(fizz);
         }
-        if (i % 5 === 0) {
+        if (i % 5 === 0 && useBuzz) {
             output.push(buzz);
         }
-        if (i % 7 === 0) {
+        if (i % 7 === 0 && useBang) {
             output.push(bang);
         }
-        if (i % 11 === 0) {
+        if (i % 11 === 0 && useBong) {
             output.length = 0;
-            if (i % 13 === 0) {
+            if (i % 13 === 0 && useFezz) {
                 output.push(fezz);
             }
             output.push(bong);
         }
-        if (i % 13 === 0) {
+        if (i % 13 === 0 && useFezz) {
             const index = output.findIndex(word => word.startsWith("B"));
             if (index === -1) {
                 output.push(fezz);
@@ -49,7 +50,7 @@ function fizzbuzz(n: number): void {
                 output.splice(index, 0, fezz);
             }
         }
-        if (i % 17 === 0) {
+        if (i % 17 === 0 && useReverse) {
             output.reverse();
         }
         const result: string = output.length === 0 ? i.toString() : output.join("");
@@ -62,7 +63,18 @@ function fizzbuzz(n: number): void {
 async function main() {
     const maxInputStr: string = await readInput('Input a number: ');
     const maxInput = parseInt(maxInputStr);
-    fizzbuzz(maxInput);
+
+    const args = process.argv.slice(2);
+    console.log("Arguments:", args);
+
+    const useFizz: boolean = args.includes("3");
+    const useBuzz: boolean = args.includes("5");
+    const useBang: boolean = args.includes("7");
+    const useBong: boolean = args.includes("11");
+    const useFezz: boolean = args.includes("13");
+    const useReverse: boolean = args.includes("17");
+
+    fizzbuzz(maxInput, useFizz, useBuzz, useBang, useBong, useFezz, useReverse);
 }
 
 main();
